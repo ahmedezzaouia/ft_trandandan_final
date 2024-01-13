2cd65d29fe3513bbe9df1311e8089f368e951ac0
adiouane <aminediouane41@gmail.com>
2024-01-13 18:53:04 +0100

    add canvas to game

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -2,7 +2,7 @@
 // Import statements...
 import { Socket } from 'socket.io'
 import { io } from 'socket.io-client';
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Head from 'next/head';
 import Ball from '@/components/ball/ball';
 import Bar from '@/components/gamebar/gamebar';
@@ -15,13 +15,11 @@ import { User } from '@/types';
 import { useStore } from 'zustand';
 
 const socket:Socket = io("http://localhost:3001");
-// const user:User | null = useUserStore((state) => state.user);
 const user:User | null = useUserStore.getState().user;
 
 const HomePage: React.FC = () => {
-// export default function HomePage() {
-  // const currentuser = JSON.parse(sessionStorage.getItem("user-store"));
-  // const user = currentuser.;
+  const cvsRef = useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
   const [isGameStarted, setGameStarted] = useState(false);
   const [selectedMap, setSelectedMap] = useState<string | null>(null);
   const [ballPosition, setBallPosition] = useState({ x: 50, y: 50 });
@@ -44,6 +42,13 @@ const HomePage: React.FC = () => {
     },
     delay: 0
   });
+  const net = {
+    x: 0,
+    y: 0,
+    width: 2,
+    height: 10,
+    color: "WHITE",
+  };
 
   const [player1Score, setPlayer1Score] = useState(0);
   const [player2Score, setPlayer2Score] = useState(0);
@@ -82,9 +87,40 @@ const HomePage: React.FC = () => {
   };
 
   useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        resetGame();
+    const cvs = cvsRef.current;
+    const ctx = cvs?.getContext("2d");
+    
+    ctxRef.current = ctx;
+    
+    if (!cvs || !ctx) return;
+    const drawNet = () => {
+      for (let i = 0; i <= cvs.height; i += 15) {
+        drawrect(cvs.width / 2 - 1, net.y + i, net.width, net.height, net.color);
+      }
+    };
+    
+    const drawrect = (x: number, y: number, w: number, h: number, color: string) => {
+      if (ctx) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, w, h);
+      }
+    };
+    
+    const drawcircle = (x: number, y: number, r: number, color: string) => {
+      if (ctx) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+      }
+    };
+    
+    const drawtext = (text: any, x: number, y: number, color: string, font: string) => {
+      if (ctx) {
+        ctx.fillStyle = color;
+        ctx.font = font;
+        ctx.fillText(text.toString(), x, y);
       }
     };
     if (string != "you can play"){
@@ -93,40 +129,56 @@ const HomePage: React.FC = () => {
         setstr(str);
       }
       socket.on('wait', queuehundler);
+      drawrect(0, 0, cvs.width, cvs.height, "BLACK");
+      drawtext("WAIT FOR OTHER PLAYER", cvs.width / 3, cvs.height / 2, "WHITE", "50px fantasy");
     }
     if (string == "you can play"){
-      setPlayer1Joined(true);
-      setPlayer2Joined(true);
       const hundler = (init : any) => {
         setStr2(init);
       };
+      
       socket.on('userposition', hundler);
+
+
       const move = (evt: any) => {
-        let pos = evt.clientY - 300;
-        // console.log(evt.clientY);
-        if (evt.clientY > 300 && evt.clientY < 900)
-          socket.emit('dataofmouse', pos);
+        let rect = cvs.getBoundingClientRect();
+        
+        let pos = evt.clientY - rect.top - 100;
+        socket.emit('dataofmouse', pos);
       }
-      window.addEventListener("mousemove", move);
-      const ballposition = {x: players.ball.x, y: players.ball.y};
-      const player1position = {x: players.user1.x, y: players.user1.y};
-      const player2position = {x: players.user2.x, y: players.user2.y};
-      if (players.delay)
-        setdelaystat(true);
-      setBallPosition(ballposition);
-      setplayer1Position(player1position);
-      setplayer2Position(player2position);
+
+      cvs.addEventListener("mousemove", move);
       setPlayer1Score(players.user1.score);
       setPlayer2Score(players.user2.score);
-      window.addEventListener('keydown', handleKeyDown);
+      drawrect(0, 0, cvs.width, cvs.height, "BLACK");
+      drawNet();
+      
+      drawtext(players.user1.score, cvs.width / 4, cvs.height / 5, "WHITE", "50px fantasy");
+      drawtext(players.user2.score, (3 * cvs.width) / 4, cvs.height / 5, "WHITE", "50px fantasy");
+      
+      drawrect(players.user1.x, players.user1.y, 10, 200, "WHITE");
+      drawrect(players.user2.x, players.user2.y, 10, 200, "WHITE");
+      
+      drawcircle(players.ball.x, players.ball.y, 15, "WHITE");
+      if (players.delay)
+        drawtext(players.delay, cvs.width / 2.5 , 2 * cvs.height / 3, "WHITE", "500px fantasy");
+      if (players.user1.status || players.user2.status){
+        let user;
+        if (players.user1.socket == socket.id)
+          user = players.user1;
+        else
+          user = players.user2;
+        if (user.status)
+          drawtext("YOU WON", cvs.width / 3 , cvs.height / 2, "WHITE", "100px fantasy");
+        else
+          drawtext("YOU LOST", cvs.width / 3 , cvs.height / 2, "WHITE", "100px fantasy");
+      }
+
       return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-        window.removeEventListener("mousemove", move);
         socket.off('userposition', hundler);
-      };
+        cvs.removeEventListener("mousemove", move);
+        };
     }
-    else
-      string += 'a';
   }, [players, string]);
 
   return (
@@ -134,32 +186,7 @@ const HomePage: React.FC = () => {
       <Head>{/* ... */}</Head>
       <main className="page">
         <div className="square">
-          {!isGameStarted ? (
-            <MapSelection
-              maps={maps}
-              onSelectMap={handleSelectMap}
-              onStartGame={handleStartGame}
-            />
-          ) : (
-            <>
-              <div>
-            <Score leftScore={player1Score} rightScore={player2Score} />
-              </div>
-            <div className="gameArea" style={{ backgroundImage: selectedMap ? `url(${maps.find((map) => map.id.toString() === selectedMap)?.imageUrl})` : 'none' }}>
-                {isPlayer1Joined && isPlayer2Joined ? (
-                  <>
-                    <Ball x={ballPosition.x} y={ballPosition.y} />
-                    <Bar position={player1Position} left={true} />
-                    <Bar position={player2Position} left={false} />
-                    {/* <Delay stat={players.delay}/> */}
-                    <div className="verticalLine"></div>
-                  </>
-              ) : (
-                <WaitStage />
-                )}
-            </div>
-          </>
-          )}
+          <canvas className="canvas-container" width="1400" height="800" ref={cvsRef} ></canvas>
         </div>
       </main>
     </div>
